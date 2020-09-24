bddf3528087396298d296c7e7bb8915e210fa73e
Aurelien <aurelien.antonio@gmail.com>
2020-09-24 05:04:20 -0500

    create swtich and refactor

--- public/index.php
+++ public/index.php
@@ -1,45 +1,30 @@
 <?php
 
-
 require '../vendor/autoload.php';
-
 require '../src/controller/controller.php';
- 
-try{
-    if(isset($_GET['route']))
-    {
-        
-        if($_GET['route'] === 'single'){
-            displaySingle();
-        }
 
-        elseif($_GET['route']==='add_Article'){
-            displayarticle();
+try {
+    if (isset($_GET['route'])) {
+        switch ($_GET['route']) {
+            case 'single':
+                displaySingle();
+                break;
+            case 'add_Article':
+                displayarticle();
+                break;
+            case 'add_comment':
+                displaycomment();
+                break;
+            case 'edit_Article':
+                updatearticle();
+                break;
+            default:
+                echo 'page inconnue';
+                break;
         }
-
-        elseif($_GET['route']==='add_comment'){
-            displaycomment();
-        }
-
-        elseif ($_GET['route']==='edit_Article'){
-            updatearticle();
-        }
-
-        else{
-            echo 'page inconnue';
-        }
-
-    }
-    else{
+    } else {
         displayHome();
     }
-   
+} catch (Exception $e) {
+    echo 'Erreur' . $e->getMessage();
 }
-
-catch (Exception $e)
-{
-    echo 'Erreur'.$e->getMessage();
-}
-
-
-?>
--- src/controller/controller.php
+++ src/controller/controller.php
@@ -1,14 +1,12 @@
 <?php
-	
+		    require '../src/DAO/DAO.php';
+			//On ajouter le fichier Article.php
+			require '../src/DAO/ArticleDAO.php';
+			//On ajouter le fichier Comment.php
+			require '../src/DAO/CommentDAO.php';
+
 	function displayHome() 
 	{
-		//On inclut le fichier dont on a besoin (ici à la racine de notre site)
-		require '../src/DAO/DAO.php';
-
-		//On ajouter le fichier Article.php
-		require '../src/DAO/ArticleDAO.php';
-		//comment ajouter un article 
-
 		/*on recupére tous nos article*/
 		$article = new ArticleDAO();
 
@@ -20,25 +18,11 @@
 
 	function displaySingle()
 	{
-
 		//On inclut le fichier dont on a besoin (ici à la racine de notre site)
-	    require '../src/DAO/DAO.php';
-	    
-	    //On ajouter le fichier Article.php
-	    require '../src/DAO/ArticleDAO.php';
-	 
-	    //On ajouter le fichier Comment.php
-	    require '../src/DAO/CommentDAO.php';
-
 	    //On recupere l'articles qu'on veut afficher grace l'attribut (GET)
 	    $article = new ArticleDAO();
-	    
        	$articles = $article->getArticle($_GET['articleId']);
-       	//echo "Hello";
-        //print_r($articles);
-        // On récupérer tous les commentaires associés à l'article
         $comment = new CommentDAO();
-
         $comments = $comment->getCommentsFromArticle($_GET['articleId']);
 
         require '../views/single.php';
@@ -46,23 +30,13 @@
 	}
  	function displayarticle()
  		{
- 			require '../src/DAO/DAO.php';
-
-			require '../src/DAO/ArticleDAO.php';
-
 			$articleDAO = new ArticleDAO();
-
 			$articleDAO->add_article();
-
 			require '../views/add_Article.php';
 
  		}
 	function  displaycomment()
 		{
-			require '../src/DAO/DAO.php';
-
-			require '../src/DAO/CommentDAO.php';
-
 			$CommentDAO = new CommentDAO();
 
 			$CommentDAO->add_comment();
@@ -72,10 +46,6 @@
 		}
 		function  updatearticle()
 		{
-			require '../src/DAO/DAO.php';
-
-			require '../src/DAO/ArticleDAO.php';
-
 			$articleDAO = new ArticleDAO();
 
 			$articleDAO->edit_Article();
@@ -83,5 +53,3 @@
 			require '../views/edit_Article.php';
 
 		}
- 	
-?>
